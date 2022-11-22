a65a9fc582db26bdb7db3fcbc114d0afc128459d
subkim <84912435+wnstjqaodls@users.noreply.github.com>
2022-11-22 22:18:00 +0900

    Add files via upload

--- mkdp-v1.0.0/src/main/java/com/mkdp/mkdpApp/HomeController.java
+++ mkdp-v1.0.0/src/main/java/com/mkdp/mkdpApp/HomeController.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.Locale;
 import java.util.Properties;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -41,7 +46,6 @@ public class HomeController {
 		} catch ( IOException e) {
 			e.printStackTrace();
 		}
-		
 		System.out.println(props.get("system.password"));
 		
 		Date date = new Date();
@@ -59,7 +63,7 @@ public class HomeController {
 	public String loginForm() {
 		System.out.println("TestController.loginForm get");
 		return "loginForm";//viewResolver
-		// /WEB-INF/views/   loginForm   .jsp 
+		// /WEB-INF/views/   loginForm.jsp 
 	}
 		
 	@RequestMapping(value = "/login", method = RequestMethod.POST)//
@@ -68,4 +72,40 @@ public class HomeController {
 		return "login";//login.jsp
 	}
 	
+	//쿠키 설정관련
+	@RequestMapping("/setCookie")
+	public String setCookie(HttpServletRequest request, HttpServletResponse response) {
+		Cookie c1= new Cookie("myName", "김준섭");
+		Cookie c2= new Cookie("age", "29");
+		Cookie c3= new Cookie("favorite", "운동");
+		c1.setMaxAge(60*60); // 쿠키의 지속시간
+		c2.setMaxAge(60*60);
+		c3.setMaxAge(60*60);
+		response.addCookie(c1);
+		response.addCookie(c2);
+		response.addCookie(c3);
+		System.out.println("set cookies==================");
+		return "home";
+	}
+	
+	@RequestMapping("/getCookie")
+	public String getCookie(HttpServletRequest request, HttpServletResponse response) {
+		Cookie[] cookies= request.getCookies();
+		for (Cookie cookie : cookies) {
+			System.out.println(cookie.getName()+"\t"+ cookie.getValue());
+		}
+		return "home";
+	}
+	
+	@RequestMapping("/getHeader")
+	public String getHeader (HttpServletRequest request, HttpServletResponse response) {
+		Enumeration<String> keys= request.getHeaderNames();
+		while (keys.hasMoreElements()) {
+			String key = (String) keys.nextElement();
+			String value= request.getHeader(key);
+			System.out.println(key+"\t"+value);			
+		}
+		return "home";
+		
+	}
 }
