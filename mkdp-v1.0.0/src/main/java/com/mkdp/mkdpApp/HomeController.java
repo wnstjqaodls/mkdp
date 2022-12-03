9f753b3686be9e957ba9d13f3d453b33fc3fa4a0
wnstj <wnstj@subkimLaptopSamsung>
2022-12-03 18:16:23 +0900

    test

--- mkdp-v1.0.0/src/main/java/com/mkdp/mkdpApp/HomeController.java
+++ mkdp-v1.0.0/src/main/java/com/mkdp/mkdpApp/HomeController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+	// add test commit 
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
